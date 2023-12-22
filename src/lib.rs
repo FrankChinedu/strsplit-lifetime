fca34b0b56f09ba7d5876ced9644d367dba15619
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-22 13:09:19 +0100

    first part

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// #![warn(missing_debug_implementations, missing_docs)]
+
+struct StrSplit<'a> {
+    remainder: &'a str,
+    delimeter: &'a str,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimeter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimeter,
+        }
     }
+}
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimeter) {
+            let until_delimeter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimeter.len())..];
+            Some(until_delimeter)
+        } else if self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = "";
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ").collect::<Vec<_>>();
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"])
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters = StrSplit::new(haystack, " ").collect::<Vec<_>>();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""])
 }
