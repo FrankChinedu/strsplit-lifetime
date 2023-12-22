6ab9f40692da3969bbfbb337da5e4821326214af
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-22 14:39:54 +0100

    life time making sense

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,46 @@
 // #![warn(missing_debug_implementations, missing_docs)]
 
-struct StrSplit<'a> {
-    remainder: &'a str,
-    delimeter: &'a str,
+struct StrSplit<'haystack, 'delimeter> {
+    remainder: Option<&'haystack str>,
+    delimeter: &'delimeter str,
 }
 
-impl<'a> StrSplit<'a> {
-    pub fn new(haystack: &'a str, delimeter: &'a str) -> Self {
+impl<'haystack, 'delimeter> StrSplit<'haystack, 'delimeter> {
+    pub fn new(haystack: &'haystack str, delimeter: &'delimeter str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimeter,
         }
     }
 }
 
-impl<'a> Iterator for StrSplit<'a> {
-    type Item = &'a str;
+impl<'haystack, 'delimeter> Iterator for StrSplit<'haystack, 'delimeter> {
+    type Item = &'haystack str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delim) = self.remainder.find(self.delimeter) {
-            let until_delimeter = &self.remainder[..next_delim];
-            self.remainder = &self.remainder[(next_delim + self.delimeter.len())..];
-            Some(until_delimeter)
-        } else if self.remainder.is_empty() {
-            None
+        if let Some(ref mut remainder) = self.remainder {
+            if let Some(next_delim) = remainder.find(self.delimeter) {
+                let until_delimeter = &remainder[..next_delim];
+                *remainder = &remainder[(next_delim + self.delimeter.len())..];
+                Some(until_delimeter)
+            } else {
+                self.remainder.take()
+            }
         } else {
-            let rest = self.remainder;
-            self.remainder = "";
-            Some(rest)
+            None
         }
     }
+}
+
+fn until_char(s: &str, c: char) -> &str {
+    StrSplit::new(s, &format!("{}", c))
+        .next()
+        .expect("StrSplit always gives at least one result")
+}
+
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("hello world", 'o'), "hell");
 }
 
 #[test]
